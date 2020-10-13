4d7387c15c18b72cd00e85fe8c66283ef61175a2
HYEZ <oohyejioo@gmail.com>
2020-10-13 13:51:21 +0900

    명령어 3개 실행

---
+++ CHAPTR07/fexecute/fexecute/fexecute.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdlib.h>
+#include "Everything.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	// fexecute 명령어1 명령어2 명령어3
+	cout << "fexecute 명령어1 명령어2 명령어3 으로 입력해주세요." << endl;
+	if (argc != 4) {
+		ReportError(_T("Usage: fexecute 명령어1 명령어2 명령어3"), 1, FALSE);
+	}
+	for (int i = 1; i <= 3; i++) {
+		//cout << _T("test");
+		system(argv[i]);
+	}
+	return 0;
+}
